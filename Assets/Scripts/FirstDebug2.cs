261edac55bf723deb4d5b886c8050a11a3bd77ef
maximaarminger <maxima.arminger@htl-salzburg.ac.at>
2019-10-03 10:08:21 +0200

    Added Assets

---
+++ Assets/Scripts/FirstDebug2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstDebug2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("First git2"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
